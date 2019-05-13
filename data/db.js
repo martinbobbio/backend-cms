e690fdee01d278be46119542386bb637f0a28e2a
Martin Bobbio <martinbobbio1@gmail.com>
2019-05-12 21:15:30 -0300

    fix for heroku

--- data/db.js
+++ data/db.js
@@ -6,9 +6,9 @@ mongoose.set('useFindAndModify', false);
 
 const env = process.argv[2] || 'dev'
 if(env === 'dev')
-    mongoose.connect('mongodb://localhost/clients', { useNewUrlParser: true })
+    mongoose.connect('mongodb://mbobbio:mbobbio1010@ds155516.mlab.com:55516/cms', { useNewUrlParser: true }, console.log("Base de datos: \x1b[32m%s\x1b[0m", "online"))
 else if(env === 'prod')
-    mongoose.connect('mongodb://mbobbio:mbobbio1010@ds155516.mlab.com:55516/cms', { useNewUrlParser: true })
+    mongoose.connect('mongodb://mbobbio:mbobbio1010@ds155516.mlab.com:55516/cms', { useNewUrlParser: true }, () => console.log("Base de datos: \x1b[32m%s\x1b[0m", "online"))
 
 const ClientsSchema = new mongoose.Schema({
     name: String,
